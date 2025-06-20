2ae476064a88bf391a1f018438ba77a3fd3776ba
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-06-20 11:39:14 +0300

    Add make_courses method to create Course objects from scraped HTML

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -5,4 +5,24 @@ import ipdb
 
 
 class Scraper:
-    pass
+    def __init__(self):
+        self.courses = []
+
+    def get_page(self):
+        return BeautifulSoup(
+            requests.get("http://learn-co-curriculum.github.io/site-for-scraping/courses").text,
+            'html.parser'
+        )
+
+    def get_courses(self):
+        return self.get_page().select('.post')
+
+    def make_courses(self):
+        for course in self.get_courses():
+            title = course.select("h2")[0].text if course.select("h2") else ''
+            schedule = course.select(".date")[0].text if course.select(".date") else ''
+            description = course.select("p")[0].text if course.select("p") else ''
+
+            new_course = Course(title, schedule, description)
+            self.courses.append(new_course)
+        return self.courses
